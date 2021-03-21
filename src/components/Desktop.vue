0ad424dbd7fc9be0ccf35a2022baf84154ef992b
Santeri Tuomisto <tuomisto.santeri.m@gmail.com>
2021-03-21 20:06:59 +0200

    Desktop items added

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -2,26 +2,38 @@
   <div id="desktop">
     <Window />
   </div>
+  <div class="desktop-wrapper">
+    <DesktopItem/>
+    <DesktopItem/>
+    <DesktopItem/>
+  </div>
 </template>
 
 <script>
 import Window from './Window.vue';
+import DesktopItem from './DesktopItem.vue';
 
 export default {
   components: {
     Window,
+    DesktopItem,
   },
 };
 </script>
 
 <style scoped>
   #desktop {
-    height: calc(100vh - 42px);
-    width: 100%;
-    background-color: gray;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 42px;
   }
 
   .desktop-wrapper {
+    height: calc(100vh - 42px);
+    width: 100%;
+    background-color: gray;
     display: flex;
     flex-direction: column;
   }
