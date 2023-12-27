30efe45650de33ec1aa78abb522a1e0f398b8dd4
Santeri Tuomisto <tuomisto.santeri.m@gmail.com>
2023-12-27 23:54:42 +0200

    boogle search

--- src/components/running/browser/webpages/Boogle.vue
+++ src/components/running/browser/webpages/Boogle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="page">
+    <div class="page" v-if="!searchOpen">
       <div class="logo">
         <span style="color: blue;">B</span>
         <span style="color: red;">o</span>
@@ -10,14 +10,60 @@
         <span style="color: red;">e</span>
       </div>
       <div>
-        <input type="text" placeholder="Search Boogle or type a URL" class="search" />
+        <input type="text" v-model="value" placeholder="Search Boogle or type a URL" class="search" style="margin-top: 20px;" />
       </div>
       <div>
-        <button>Search</button>
+        <button @click="() => search(value)">Search</button>
+      </div>
+    </div>
+    <div v-else class="search-page">
+      <div class="search-bar">
+        <div class="logo">
+          <span style="color: blue;">B</span>
+          <span style="color: red;">o</span>
+          <span style="color: rgb(255, 187, 18);">o</span>
+          <span style="color: blue;">g</span>
+          <span style="color: green;">l</span>
+          <span style="color: red;">e</span>
+        </div>
+        <div>
+          <input type="text" v-model="value" placeholder="Search Boogle or type a URL" class="search" />
+          <button @click="() => search(value)">Search</button>
+        </div>
+      </div>
+      <div class="results">
+        <div>
+          Did you mean: asdasd
+        </div>
+        <div>No results containing all your search terms were found.</div>
+        <div>Your search - {{ searchedWord }} - did not match any documents.</div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '',
+      searchOpen: false,
+      searchedWord: '',
+    };
+  },
+  methods: {
+    search(searchValue) {
+      if (searchValue !== '') {
+        this.searchOpen = true;
+        this.searchedWord = searchValue;
+      } else {
+        this.searchOpen = false;
+        this.searchWord = '';
+      }
+    },
+  },
+};
+</script>
 
 <style scoped lang="scss">
 .container {
@@ -40,9 +86,30 @@
 }
 
 .search {
-  margin-top: 20px;
   min-width: 250px;
   width: 70%;
   font-size: 17px;
+}
+
+.search-page {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.search-bar {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  padding-left: 10px;
+}
+
+.results {
+  color: black;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 10px;
 }
 </style>
